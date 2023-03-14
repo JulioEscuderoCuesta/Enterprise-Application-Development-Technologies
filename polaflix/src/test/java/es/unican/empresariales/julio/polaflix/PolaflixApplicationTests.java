81105ef191a42a1bd9ce3768f0bf047ca1431ecf
JulioEscuderoCuesta <escuderocuestajulio@gmail.com>
2023-03-14 20:51:24 +0100

    Estructura inical de acuerdo al modelo de dominio completada (Menos herencia en Serie --> se añade clase Categoria

---
+++ polaflix/src/test/java/es/unican/empresariales/julio/polaflix/PolaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.empresariales.julio.polaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
