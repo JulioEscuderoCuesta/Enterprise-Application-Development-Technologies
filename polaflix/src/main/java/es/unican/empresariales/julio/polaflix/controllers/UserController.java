2ef9a7f97d693316dde00faebbd9350460e6cd71
JulioEscuderoCuesta <escuderocuestajulio@gmail.com>
2023-05-04 13:02:52 +0200

    Documento con la especificación de la API REST. Commit para nueva rama

---
+++ polaflix/src/main/java/es/unican/empresariales/julio/polaflix/controllers/UserController.java
@@ -0,0 +1,12 @@
+package es.unican.empresariales.julio.polaflix.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+    
+}
