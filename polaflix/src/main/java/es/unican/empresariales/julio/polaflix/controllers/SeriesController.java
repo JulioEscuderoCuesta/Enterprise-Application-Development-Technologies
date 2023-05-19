01264189d1eebbbc8a8131c288069b7814ac0486
JulioEscuderoCuesta <escuderocuestajulio@gmail.com>
2023-05-19 12:05:34 +0200

    Añadido CrossOrigin

--- polaflix/src/main/java/es/unican/empresariales/julio/polaflix/controllers/SeriesController.java
+++ polaflix/src/main/java/es/unican/empresariales/julio/polaflix/controllers/SeriesController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import es.unican.empresariales.julio.polaflix.services.SeriesService;
 
 @RestController
 @RequestMapping("/series")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SeriesController {
     
     @Autowired
